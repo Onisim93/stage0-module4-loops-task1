7e73622703af69b2c560ff8c9ec66a7aac948c59
onisim_albarov <onisim93@gmail.com>
2022-11-29 08:25:27 +0400

    complete module4-loops-task1

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,26 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        String text = "writing the same code doesn't have much impact, and it's also time consuming";
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
     }
 }
